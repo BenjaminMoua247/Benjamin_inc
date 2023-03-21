02ecf31eafeec17c8f8da6ebe577262bdf0357bf
Benjamin Moua <112416412+BenjaminMoua247@users.noreply.github.com>
2023-03-21 17:08:38 +0000

    Tue, Mar 21, 2023, 10:08 AM -07:00

--- js/counter.js
+++ js/counter.js
@@ -1,1 +1,17 @@
 const counters = document.querySelectorAll('.counter')
+
+counters.forEach(counter => {
+    counter.innerText = '0'
+
+    const updateCounter = () => {
+        const target = +counter.getAttribute('data-target')
+        console.log(target)
+        const c = +counter.innerText
+
+        const increment = target / 200
+
+        if (c < target) {
+            counter.innerText = `${Math.ceil(c + increment)}`
+        }
+    }
+})
