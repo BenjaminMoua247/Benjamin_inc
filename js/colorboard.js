633799fc8b9ad1993138d29adde4c63c585b2a84
Benjamin Moua <112416412+BenjaminMoua247@users.noreply.github.com>
2023-03-22 16:53:28 +0000

    Wed, Mar 22, 2023, 9:53 AM -07:00

--- js/colorboard.js
+++ js/colorboard.js
@@ -1,6 +1,6 @@
 const container = document.getElementById('container')
 const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71']
-const SQUARES = 4000
+const SQUARES = 10000
 
 
 for(let i = 0;i < SQUARES; i++) {
