9363affac088d493da19013fb544d7ffef943ae7
Benjamin Moua <112416412+BenjaminMoua247@users.noreply.github.com>
2023-03-22 16:45:09 +0000

    Wed, Mar 22, 2023, 9:45 AM -07:00

--- js/colorboard.js
+++ js/colorboard.js
@@ -3,7 +3,7 @@ const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71']
 const SQUARES = 500
 
 
-for (let i = 0; i < SQUARES; i++) {
+for(let i = 0;i < SQUARES; i++) {
     const square = document.createElement('div')
     square.classList.add('square')
     container.appendChild(square)
@@ -12,15 +12,21 @@ for (let i = 0; i < SQUARES; i++) {
 }
 
 
+
+
 function removeColor(element) {
     element.style.background = '#1d1d1d'
 }
+
+
 
 
 function setColor(element) {
     const color = getRandomColor()
     element.style.background = color
 }
+
+
 
 
 function getRandomColor() {
