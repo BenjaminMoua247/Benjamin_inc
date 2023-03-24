61b744f0436faee6919cfe1665ab11e8f570f501
Benjamin Moua <112416412+BenjaminMoua247@users.noreply.github.com>
2023-03-24 16:43:48 +0000

    Fri, Mar 24, 2023, 9:43 AM -07:00

--- js/typing.js
+++ js/typing.js
@@ -3,3 +3,16 @@ const speedEl = document.getElementById('speed')
 const text = 'Bem-00sk'
 let idx=1
 let speed = 300/speedEl.val
+
+writeText()
+
+function writeText(){
+    textEl.innerText = text.slice(0,idx)
+    idx++
+
+    if (idx > text.length) {
+        idx = 1
+    }
+
+    setTimeout(write)
+}
