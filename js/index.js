d4e1708a84bbb6b5e04bbaf99c914ee2fcc5dc05
Benjamin Moua <112416412+BenjaminMoua247@users.noreply.github.com>
2023-03-28 16:38:43 +0000

    Tue, Mar 28, 2023, 9:38 AM -07:00

--- js/index.js
+++ js/index.js
@@ -1,6 +1,4 @@
-const search = document.querySelector('.search')
 const btn = document.querySelector('.btn')
-const input = document.querySelector('.input')
 
 btn.addEventListener('click', () => {
     search.classList.toggle('active')
